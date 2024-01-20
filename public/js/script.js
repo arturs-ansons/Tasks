9053ebcec662fba49751fc5cb2e671f74d32da7b
ansonsarturs <65274330+arturs-ansons@users.noreply.github.com>
2024-01-20 15:38:50 +0200

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,28 @@
-
 function submitForm(event) {
     event.preventDefault();
 
     fetch('/insert', {
-    method: 'POST',
-    body: new FormData(document.forms.insertForm),
-})
-    .then(response => {
-    if (response.ok) {
-    location.reload();
-} else {
-    return response.json();
-}
-})
-    .then(data => handleResponse(data))
-    .catch(error => console.error('Error:', error));
+        method: 'POST',
+        body: new FormData(document.forms.insertForm),
+    })
+        .then(response => {
+            if (response.ok) {
+                location.reload();
+            } else {
+                return response.json();
+            }
+        })
+        .then(data => handleResponse(data))
+        .catch(error => console.error('Error:', error));
 }
 
- function handleResponse(data) {
+function handleResponse(data) {
     const errorMessageElement = document.getElementById('errorMessage');
 
     if (data.validationMessage) {
-    errorMessageElement.style.display = 'block';
-    errorMessageElement.innerHTML = data.validationMessage;
-} else {
-    errorMessageElement.style.display = 'none';
+        errorMessageElement.style.display = 'block';
+        errorMessageElement.innerHTML = data.validationMessage;
+    } else {
+        errorMessageElement.style.display = 'none';
+    }
 }
-}
-
